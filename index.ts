496f065f435450d204c1713491bd29aca99f96ad
Ethan Pletcher <ethan@Ethans-MacBook-Air.local>
2025-01-29 15:51:44 -0500

    rm-export

--- index.ts
+++ index.ts
@@ -2,8 +2,4 @@ import { NativeModules } from "react-native";
 
 const InstalledAppList = NativeModules.InstalledAppList;
 
-module.exports = {
-  getAll: InstalledAppList.getAll,
-};
-
 export default InstalledAppList;
